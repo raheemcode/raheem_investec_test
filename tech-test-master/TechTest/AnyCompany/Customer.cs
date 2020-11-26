64e8e6eafca8f7e4e74f310643976c2b544ebfe3
raheemcode <75081041+raheemcode@users.noreply.github.com>
2020-11-26 16:05:17 +0200

    Add files via upload

---
+++ tech-test-master/TechTest/AnyCompany/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AnyCompany
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Country { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ tech-test-master/TechTest/AnyCompany/CustomerRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AnyCompany
+{
+    public static class CustomerRepository
+    {
+        private static string ConnectionString = @"Data Source=.\SQLEXPRESS;Database=Customers;User Id=sa;Password=P@ssword365;";
+
+        public static Customer Load(int customerId)
+        {
+            Customer customer = new Customer();
+
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Customers WHERE CustomerId = " + customerId,
+                connection);
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                customer.CustomerId = int.Parse(reader["CustomerId"].ToString());
+                customer.Name = reader["Name"].ToString();
+                customer.DateOfBirth = DateTime.Parse(reader["DateOfBirth"].ToString());
+                customer.Country = reader["Country"].ToString();
+            }
+
+            connection.Close();
+
+            return customer;
+        }
+    }
+}
